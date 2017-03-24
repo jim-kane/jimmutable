0cce0deb6bf3d87400193d69a90e373b29c789a6
Jim Kane <jim.kane@gmail.com>
2017-03-24 15:44:57 -0700

    Modified package structure

--- base/src/main/java/org/kane/base/decks/StandardImmutableListDeck.java
+++
@@ -1,9 +0,0 @@
-package org.kane.base.decks;
-
-import org.kane.base.fields.FieldList;
-
-abstract public class StandardImmutableListDeck<T extends StandardImmutableListDeck<T, E>, E> extends StandardImmutableDeck<T, E>
-{
-	@Override
-    abstract public FieldList<E> getSimpleContents();
-}
--- base/src/main/java/org/kane/base/decks/StandardImmutableSetDeck.java
+++
@@ -1,9 +0,0 @@
-package org.kane.base.decks;
-
-import org.kane.base.fields.FieldSet;
-
-abstract public class StandardImmutableSetDeck<T extends StandardImmutableSetDeck<T, E>, E> extends StandardImmutableDeck<T, E>
-{
-	@Override
-    abstract public FieldSet<E> getSimpleContents();
-}
